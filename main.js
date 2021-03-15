e6f50b99e64b8eeaf1820c05139bb46f9ef01cb1
Leninus <Leninus.spr@gmail.com>
2021-03-15 11:14:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 var gameData = 
 {
     gold: 0,
-    gpc: 1
+    gpc: 1,
+    gpccost: 10
 }
+var mainGameLoop = window.setInterval(function() 
+{
+    mineGold()
+}, 1000)
+
 function mineGold()
 {
     gameData.gold += gameData.gpc 
     document.getElementById("gold").innerHTML = gameData.gold + " Gold Mined"
+}
+function buyGPC()
+{
+    if (gameData.gold >= gameData.gpccost)
+    {
+        gameData.gold -= gameData.gpccost
+        gameData.gpc += 1
+        gameData.gpccost *= 2
+        document.getElementById("gold").innerHTML = gameData.gold + " Gold Miend"
+        document.getElementById("pCUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.gpc + ") Cost: " + gameData.gpccost + " Gold"
+    }
 }
