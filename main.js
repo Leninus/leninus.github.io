2c38567521eba613abb839548af5c6ebb0b44850
Leninus <Leninus.spr@gmail.com>
2021-03-15 10:57:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ var gameData =
 function mineGold()
 {
     gameData.gold += gameData.gpc 
-    document.getElementById("gold").innerHTML = "${gameData.gold} Gold Mined"
+    document.getElementById("gold").innerHTML = gameData.gold + " Gold Mined"
 }
