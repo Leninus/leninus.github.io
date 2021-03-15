cf68a6138dffd98bc3f9edff11e2968520c8c6aa
Leninus <Leninus.spr@gmail.com>
2021-03-15 11:21:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,15 @@ var mainGameLoop = window.setInterval(function()
 {
     mineGold()
 }, 1000)
+var saveGameLoop = window.setInterval(function()
+{
+    localStorage.setItem("goldMinerSave", JSON.stringify(gameData))
+}, 15000)
+var savegame = JSON.parse(localStorage.getItem("goldMinerSave"))
+if (savegame !== null)
+{
+    gameData = savegame
+}
 
 function mineGold()
 {
